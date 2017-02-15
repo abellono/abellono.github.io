183eea532a299721716d1757d9a2b03e8e24b5e3
Hakon Hanesand <hakon@abello.no>
2017-02-15 16:01:54 -0600

    updates to sample script and jekyll data file creation

--- sample_buddybuild_postbuild.sh
+++ sample_buddybuild_postbuild.sh
@@ -11,7 +11,7 @@
 #		Folder may not exist until first build has been uploaded, but it is ok to generate IPA first then place screenshots in correct folder
 # 6. Change TITLE variable below to correct value
 
-INFO_PLIST_PATH="./app/Supporting Files/roomservice-ios-driver-Info.plist" 
+INFO_PLIST_PATH="./app/Supporting Files/roomservice-ios-driver-Info.plist"
 BRANCH_TO_UPLOAD="master"
 TITLE="Roomservice"
 
@@ -24,13 +24,15 @@ cd "$BUDDYBUILD_WORKSPACE" || exit
 if [ "$BUDDYBUILD_BRANCH" == "$BRANCH_TO_UPLOAD" ]; then
 
 	echo "Uploading $BUDDYBUILD_BRANCH."
-	
+
 	BUILD_VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBuildVersion" "$INFO_PLIST_PATH")
 	BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBuildNumber" "$INFO_PLIST_PATH")
 	BUNDLE_IDENTIFIER=$(/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier" "$INFO_PLIST_PATH")
 
 	UPLOAD_FOLDER_DIR="upload-to-github"
 	BUILD_PRODUCTS_DIR="builds"
+
+	BASE_REPO_PATH="$BUDDYBUILD_WORKSPACE"/"$UPLOAD_FOLDER_DIR"
 
 	# Create and change into upload folder that we will copy the IPA into
 	git clone -b master --depth 1 git@github.com:abellono/abello-web.git $UPLOAD_FOLDER_DIR
@@ -64,6 +66,32 @@ if [ "$BUDDYBUILD_BRANCH" == "$BRANCH_TO_UPLOAD" ]; then
 	sed -ie "s/$BUNDLE_VERSION_REPLACE_STRING/$BUILD_VERSION/g" ./manifest.plist
 	sed -ie "s/$BUNDLE_IDENTIFIER_REPLACE_STRING/$BUNDLE_IDENTIFIER/g" ./manifest.plist
 	sed -ie "s/$TITLE_REPLACE_STRING/$TITLE/g" ./manifest.plist
+
+	cd "$BASE_REPO_PATH" || exit
+
+    # If this is the first build of this app, add it to the top level app data file
+	if [ -z $(grep "$BUNDLE_IDENTIFIER" "_data/app_list.csv") ]; then
+	   echo "$BUNDLE_IDENTIFIER" >> "_data/app_list.csv"
+	fi
+
+	mkdir "_data/$BUNDLE_IDENTIFIER/"
+
+	APP_BUILD_DATA_FILE="_data/$BUNDLE_IDENTIFIER/$TITLE-$BUILD_VERSION.$BUILD_NUMBER-$BUDDYBUILD_BUILD_ID.json"
+	cp "default_app_build_data.json" "$APP_BUILD_DATA_FILE"
+
+	NAME_REPLACE_STRING=@@@@LINK@@@@
+	BUNDLE_ID_REPLACE_STRING=@@@@BUNDLE_ID@@@@
+	VERSION_REPLACE_STRING=@@@@VERSION@@@@
+	BUILD_NUMBER_REPLACE_STRING=@@@@BUILD@@@@
+	MANIFEST_REPLACE_STRING=@@@@MANIFEST@@@@
+
+	MANIFEST_LOCATION="$BASE_REPO_PATH/$BUILD_PRODUCTS_DIR/$CURRENT_BUILD_DEST_DIR/manifest.plist"
+
+	sed -ie "s/$NAME_REPLACE_STRING/$TITLE/g" "$APP_BUILD_DATA_FILE"
+	sed -ie "s/$BUNDLE_ID_REPLACE_STRING/$BUNDLE_IDENTIFIER/g" "$APP_BUILD_DATA_FILE"
+	sed -ie "s/$VERSION_REPLACE_STRING/$BUILD_VERSION/g" "$APP_BUILD_DATA_FILE"
+	sed -ie "s/$MANIFEST_REPLACE_STRING/$MANIFEST_LOCATION/g" "$APP_BUILD_DATA_FILE"
+	sed -ie "s/$BUILD_NUMBER_REPLACE_STRING/$BUILD_NUMBER/g" "$APP_BUILD_DATA_FILE"
 
 	git add ./*
 	git commit -m "Buddybuild: Uploaded IPA for $BUNDLE_IDENTIFIER -> $BUILD_VERSION.$BUILD_NUMBER"
