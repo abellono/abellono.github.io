47c1d9ead95a1363e4339e4736a591f321178d82
Hakon Hanesand <hakon@abello.no>
2017-02-21 13:31:49 -0600

    Use GitHub releases to host app pictures too.

--- defaults/default_buddybuild_postbuild.sh
+++ defaults/default_buddybuild_postbuild.sh
@@ -22,14 +22,45 @@ function upload_ipa_to_release {
 
     FILE_NAME=$(basename "$BUDDYBUILD_IPA_PATH")
     GH_ASSET="https://uploads.github.com/repos/$BUILD_REPO_NAME/releases/$id/assets"
-    GH_ASSET="$GH_ASSET?name=$FILE_NAME"
 
+    GH_ASSET_NAME="$GH_ASSET?name=$FILE_NAME"
     response=$(curl -H "Authorization: token d057bbc3f02b5c5284b97b826cacb6d99f1617ad" \
                     -H "Content-Type: application/octet-stream" \
      	            -H "Accept: application/vnd.github.v3+json" \
-                    --data-binary "@$BUDDYBUILD_IPA_PATH" "$GH_ASSET")
+                    --data-binary "@$BUDDYBUILD_IPA_PATH" "$GH_ASSET_NAME")
 
     browser_download_url=$(echo "$response" | jsonpath '$.browser_download_url' | tr -d '"[]')
+
+    GH_ASSET_NAME="$GH_ASSET?name=57.png"
+    response=$(curl -H "Authorization: token d057bbc3f02b5c5284b97b826cacb6d99f1617ad" \
+                    -H "Content-Type: image/png" \
+     	            -H "Accept: application/vnd.github.v3+json" \
+                    --data-binary "@./pictures/$BUNDLE_IDENTIFIER/57.png" "$GH_ASSET_NAME")
+
+    small_image_download_url=$(echo "$response" | jsonpath '$.browser_download_url' | tr -d '"[]')
+
+    GH_ASSET_NAME="$GH_ASSET?name=512.png"
+    response=$(curl -H "Authorization: token d057bbc3f02b5c5284b97b826cacb6d99f1617ad" \
+                    -H "Content-Type: image/png" \
+     	            -H "Accept: application/vnd.github.v3+json" \
+                    --data-binary "@./pictures/$BUNDLE_IDENTIFIER/512.png" "$GH_ASSET_NAME")
+
+    large_image_download_url=$(echo "$response" | jsonpath '$.browser_download_url' | tr -d '"[]')
+}
+
+function upload_manifest_to_release {
+
+    PATH="./manifest.plist"
+    FILE_NAME=$(basename "$PATH")
+    GH_ASSET="https://uploads.github.com/repos/$BUILD_REPO_NAME/releases/$id/assets"
+    GH_ASSET="$GH_ASSET?name=$FILE_NAME"
+
+    response=$(curl -H "Authorization: token d057bbc3f02b5c5284b97b826cacb6d99f1617ad" \
+                    -H "Content-Type: application/xml" \
+     	            -H "Accept: application/vnd.github.v3+json" \
+                    --data-binary "@$PATH" "$GH_ASSET")
+
+    manifest_download_url=$(echo "$response" | jsonpath '$.browser_download_url' | tr -d '"[]')
 }
 
 # Make sure we are in the right directory
@@ -42,7 +73,6 @@ if [ "$BUDDYBUILD_BRANCH" == "$BRANCH_TO_UPLOAD" ]; then
     echo "Uploading $BUDDYBUILD_BRANCH."
 
     INFO_PLIST_PATH="$BUDDYBUILD_PRODUCT_DIR/Info.plist"
-
     BUILD_VERSION=$(/usr/libexec/PlistBuddy -c "Print :ApplicationProperties:CFBundleShortVersionString" "$INFO_PLIST_PATH")
     BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print :ApplicationProperties:CFBundleVersion" "$INFO_PLIST_PATH")
     BUNDLE_IDENTIFIER=$(/usr/libexec/PlistBuddy -c "Print :ApplicationProperties:CFBundleIdentifier" "$INFO_PLIST_PATH")
@@ -54,11 +84,9 @@ if [ "$BUDDYBUILD_BRANCH" == "$BRANCH_TO_UPLOAD" ]; then
     fi
 
     UPLOAD_FOLDER_DIR="upload-to-github"
-    BUILD_PRODUCTS_DIR="builds"
     BASE_REPO_PATH="$BUDDYBUILD_WORKSPACE"/"$UPLOAD_FOLDER_DIR"
     DEFAULTS_FOLDER="$BASE_REPO_PATH/defaults"
 
-    # Create and change into upload folder that we will copy the IPA into
     git clone -b master --depth 1 "git@github.com:$REPO_NAME.git" "$UPLOAD_FOLDER_DIR"
     cd "$UPLOAD_FOLDER_DIR" || exit
 
@@ -70,33 +98,29 @@ if [ "$BUDDYBUILD_BRANCH" == "$BRANCH_TO_UPLOAD" ]; then
     # Creates github release and defines the release id as the id environment variable
     create_github_release || exit
 
-    # Create the build folder if it does not exist and cd into it
-    [ -d $BUILD_PRODUCTS_DIR ] || mkdir $BUILD_PRODUCTS_DIR
-    cd $BUILD_PRODUCTS_DIR || exit
-
-    # Create the product folder - BUDDYBUILD_BUILD_ID is unique, so we won't overwrite
-    CURRENT_BUILD_DEST_DIR="$BUNDLE_IDENTIFIER/$VERSION/$BUDDYBUILD_BUILD_ID"
-    mkdir -p "$CURRENT_BUILD_DEST_DIR"/
-    cd "$CURRENT_BUILD_DEST_DIR"/ || exit
-
     # Upload the IPA to the github release
     upload_ipa_to_release || exit
 
     # Copy the template manifest file into the current folder with another name
-    cp "$DEFAULTS_FOLDER"/manifest_default.plist "./manifest.plist"
+    cp "./defaults/manifest_default.plist" ./manifest.plist
 
     # Replace default values in plist
     LINK_REPLACE_STRING=@@@@LINK@@@@
     BUNDLE_VERSION_REPLACE_STRING=@@@@VERSION@@@@
     BUNDLE_IDENTIFIER_REPLACE_STRING=@@@@BUNDLE_IDENTIFIER@@@@
     NAME_REPLACE_STRING=@@@@NAME@@@@
+    SMALL_PICTURE_REPLACE_STRING=@@@@SMALL_PIC@@@@
+    LARGE_PICTURE_REPLACE_STRING=@@@@LARGE_PIC@@@@
 
     sed -i '' -e "s|$LINK_REPLACE_STRING|$browser_download_url|g" ./manifest.plist
     sed -i '' -e "s/$BUNDLE_VERSION_REPLACE_STRING/$BUILD_VERSION/g" ./manifest.plist
     sed -i '' -e "s/$BUNDLE_IDENTIFIER_REPLACE_STRING/$BUNDLE_IDENTIFIER/g" ./manifest.plist
     sed -i '' -e "s/$NAME_REPLACE_STRING/$NAME/g" ./manifest.plist
+    sed -i '' -e "s/$SMALL_PICTURE_REPLACE_STRING/$small_image_download_url/g" ./manifest.plist
+    sed -i '' -e "s/$LARGE_PICTURE_REPLACE_STRING/$large_image_download_url/g" ./manifest.plist
 
-    cd "$BASE_REPO_PATH" || exit
+    upload_manifest_to_release || exit
+    rm ./manifest.plist
 
     # Create the data folder if it does not exist
     [ -d "./_data" ] || mkdir "./_data"
@@ -106,12 +130,11 @@ if [ "$BUDDYBUILD_BRANCH" == "$BRANCH_TO_UPLOAD" ]; then
 
     BUILD_NUMBER_REPLACE_STRING=@@@@BUILD@@@@
     MANIFEST_REPLACE_STRING=@@@@MANIFEST@@@@
-    MANIFEST_LOCATION="https://github.com/abellono/abellono.github.io/blob/master/$BUILD_PRODUCTS_DIR/$CURRENT_BUILD_DEST_DIR/manifest.plist"
 
     sed -i '' -e "s/$NAME_REPLACE_STRING/$NAME/g" "$APP_BUILD_DATA_FILE"
     sed -i '' -e "s/$BUNDLE_IDENTIFIER_REPLACE_STRING/$BUNDLE_IDENTIFIER/g" "$APP_BUILD_DATA_FILE"
     sed -i '' -e "s/$BUNDLE_VERSION_REPLACE_STRING/$BUILD_VERSION/g" "$APP_BUILD_DATA_FILE"
-    sed -i '' -e "s|$MANIFEST_REPLACE_STRING|$MANIFEST_LOCATION|g" "$APP_BUILD_DATA_FILE"
+    sed -i '' -e "s|$MANIFEST_REPLACE_STRING|$manifest_download_url|g" "$APP_BUILD_DATA_FILE"
     sed -i '' -e "s/$BUILD_NUMBER_REPLACE_STRING/$BUILD_NUMBER/g" "$APP_BUILD_DATA_FILE"
 
     git add ./*
